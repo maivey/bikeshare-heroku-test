d1c13179744fca29eb7abf76a27586320f80bb11
Morgan Ivey <morganivey@Morgans-MacBook-Pro.local>
2020-06-17 16:19:22 -0700

    Update app.py

--- bikeshare_app/app.py
+++ bikeshare_app/app.py
@@ -23,9 +23,9 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
-    CITY_DATA = { 'chicago': 'data/chicago.csv',
-              'new york city': 'data/new_york_city.csv',
-              'washington': 'data/washington.csv' }
+    CITY_DATA = { 'chicago': '/data/chicago.csv',
+              'new york city': '/data/new_york_city.csv',
+              'washington': '/data/washington.csv' }
     df = pd.read_csv(CITY_DATA[city.lower()])
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 
